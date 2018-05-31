b7d12c5f60b84042e0cb04ed3379c7d5fab2676b
Jelena-Milev <milev816@fon.bg.ac.rs>
2018-06-01 00:35:24 +0200

    Dodata implementacija interfejsa java.util.Map

--- src/stablo/BinarnoStablo.java
+++ src/stablo/BinarnoStablo.java
@@ -1,5 +1,81 @@
 package stablo;
 
-public class BinarnoStablo{
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class BinarnoStablo<T> implements Map<Integer, T>{
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<Entry<Integer, T>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<Integer> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T put(Integer key, T value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends Integer, ? extends T> m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<T> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
